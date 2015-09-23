bd1b9078bb9722794599f322e78baee1156b1feb
Jesus <chuy8jay@gmail.com>
2015-09-23 13:41:17 -0600

    Part 5 java version completed. Part 5 needs more testing

--- src/cs3100_java/Part5.java
+++ src/cs3100_java/Part5.java
@@ -1,5 +1,30 @@
 package cs3100_java;
 
-public class Part5 {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class Part5 {
+	public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+
+        String input = "";
+        while(s.hasNext())
+        {
+            input += s.nextLine() + "\n";
+        }
+
+        Pattern htmlP = Pattern.compile("((<)((?!\")(?!<).|\\n)*?(>))");
+
+        Matcher m = htmlP.matcher(input);
+
+        String mResults = m.replaceAll("");
+
+        Pattern finalP = 
+        		Pattern.compile("((<)(((?!<).|\\n)*?)((\"(\\.)*.*?(\\.)*(?<!\\\\)\")|(\"(\\.)*.*?(\\.)*(?<=\\\\\\\\)\"))(((?!<).|\\n)*?)(>))");
+
+        m = finalP.matcher(mResults);
+        System.out.println(m.replaceAll("") + "\n\n\n");
+    }
 }
