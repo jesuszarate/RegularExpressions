1b1b1188e2c7e92a06a9c29c55f3005deb7b3583
Jesus <chuy8jay@gmail.com>
2015-09-23 11:15:49 -0600

    Part5: Able to print only strings with properly formatted "

--- bin/cs3100_perl/test.pl
+++ bin/cs3100_perl/test.pl
@@ -2,59 +2,26 @@ while(<STDIN>)
 {
 	$input .= $_;
 }
+$HTML = "((\<)((?!\")(?!<).|\n)*?(\>))";
+$htmls = "((\<)((?!<).|\n)*?(\>))";
+#$quoted = "(\"(\.)*.*?(\.)*(?<!\\)\")|(\"(\.)*.*?(\.)*(?<=\\\\)\"))";
+#$notHTML = "((?!<)(.|\n)(?!>))*";
 
-#$input =~ s@\<(.|\n)*?\>@@gi; 
-#$input =~ s@((\<)((?!<).|\n)*?(\>))@@gi; #Matches the case where no unquoted < characters should appear inside a tag
+$input =~ s@$HTML@@g; 
+#$input =~ s@$notHTML@@g; 
+
+
+#while($input =~ m@(\"(\.)*.*?(\.)*(?<!\\)\")|(\"(\.)*.*?(\.)*(?<=\\\\)\")@g)
+#{}
+#$input =~ s@((\<)((?!")(?!<).|\n)*?(\>))@@gi; #Matches the case where no unquoted < characters should appear inside a tag
 										 # but fails when there is two nested like: < hello <world>>
 										 
-#while($input =~ m@((\<)((?!<).|\n)*?(\>))|(.*(?!<)|(?!>))@g)
-while($input =~ m@(.)*@g)
+while($input =~ m@$htmls@g)
+#if($input =~ m@$quoted@g)
 {
-	if($input =~ m@((\<)((?!<).|\n)*?(\>))@g)
-	{
-		print "true";
-	}
-	else{
-		print "false";
-	}
-	
-	#$input =~ s@((\<)((?!<).|\n)*?(\>))@@g;
-	if(defined $one)
-	{
-		$one = $1;
-	}
-	else{
-		$one = $5;
-	}
-	
-	
-	#print $input; 
-	print "\n" . "=>" . $one . "<=" ."\n";
-	
-	if($one =~ m@((\<)((?!<).|\n)*?(\>))@g)
-	{
-		#If it contains a quote then make sure there is a matching quote
-		if($1 =~ m@"@g)
-		{
-			print $one . "\n\n";				
-			if(($one =~ m@("(\.)*.*?(\.)*(?<!\\)")|("(\.)*.*?(\.)*(?<=\\\\)")@g))
-			{
-				$input =~ s@((\<)((?!<).|\n)*?(\>))@@g;
-				#print "=>" . $input . "<="; 
-				$result .= $one;
-			}
-			else{
-				#$input =~ s@((\<)((?!<).|\n)*?(\>))@@g;
-				$result .= $one;
-			}
-		}
-	}
-	#else{
-	#	$one =~ s@((\<)((?!<).|\n)*?(\>))@@g;
-	#	#print "=> " . $input . " <="; 
-	#	$result .= $1;	
-	#}
+	#print $1;	
 }
 
-$result .= "\nend";
-#print $result;
+$input =~ s@(<((.|\n)*?(".*").*?)>)@@g;
+
+print $input;
